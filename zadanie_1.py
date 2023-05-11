959731eb2cd563800f5832cd5bf8092715d74bca
Marat <brutto061@gmail.com>
2023-05-11 21:39:19 +0700

    second commit

--- zadanie_1.py
+++ zadanie_1.py
@@ -6,23 +6,32 @@
 # переменная). Произвести расчет как с использованием intercept, так и без.
 
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 zp = np.array([35, 45, 190, 200, 40, 70, 54, 150, 120, 110])
 ks = np.array([401, 574, 874, 919, 459, 739, 653, 902, 746, 832])
 
 b1 = (len(zp)*np.sum(zp*ks) - np.sum(zp) * np.sum(ks))/ (len(zp)*np.sum(zp**2) - np.sum(zp)**2)
-print('Коэффициент b1 =', b1)
+print('Коэффициент b1 =', round(b1, 2))
 
 b0 = np.mean(ks) - b1 * np.mean(zp)
-print('Коэффициент b0 =', b0)
+print('Коэффициент b0 =', round(b0, 2))
+print()
 
 y_pred = b0 + (b1 * zp)
-print(y_pred)
+df = pd.DataFrame({'реальный скоринг': ks, 'предсказанный': y_pred})
+print(df)
 print()
 
 model = LinearRegression()
 zp = zp.reshape(-1, 1)
 regres = model.fit(zp, ks)
-print(regres.intercept_)
-print(regres.coef_)
+print('Коэффициент b1 из модели =', round(*regres.coef_, 2))
+print('Коэффициент b0 через интерсепт =', round(regres.intercept_, 2))
+print()
+
+mse = ((ks - y_pred)**2).sum()/ len(ks)
+print('Функция потерь mse =', round(mse, 2))
+print('Коэффициент детерминации R2 =', round(regres.score(zp, ks), 2))
+
