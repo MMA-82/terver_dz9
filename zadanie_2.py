df896b07f737afc6afa596c5d2567e24616eb79a
Marat <brutto061@gmail.com>
2023-05-11 22:20:30 +0700

    вторая задача

---
+++ zadanie_2.py
@@ -0,0 +1,19 @@
+# Задача 2 Посчитать коэффициент линейной регрессии при заработной плате (zp), 
+# используя градиентный спуск (без intercept).
+
+import numpy as np
+
+zp = np.array([35, 45, 190, 200, 40, 70, 54, 150, 120, 110])
+ks = np.array([401, 574, 874, 919, 459, 739, 653, 902, 746, 832])
+
+def mse_(b1, y = ks, x = zp, n = len(zp)):
+    return np.sum((b1 * x - y)**2)/ n
+
+alpha = 1e-6
+b1 = 400
+
+for i in range (1000):
+    b1 -= alpha * (2/len(zp)) * np.sum((b1 * zp - ks) * zp)
+    if i % 50 == 0:
+        print('b1 = {}, mse = {}'.format(b1, mse_(b1)))
+
